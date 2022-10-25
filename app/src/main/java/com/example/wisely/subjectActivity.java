81fcff7b38025c50f1daa398f4e65dc054bd40c5
salim84miya <114942352+salim84miya@users.noreply.github.com>
2022-10-25 18:15:06 +0530

    subjectActivity added

---
+++ app/src/main/java/com/example/wisely/subjectActivity.java
@@ -0,0 +1,15 @@
+package com.example.wisely;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class subjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.videos_subject_activity);
+    }
+}
