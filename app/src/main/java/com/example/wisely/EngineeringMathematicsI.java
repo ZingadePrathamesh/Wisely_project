a29f2067226d88564ccd318f48a064cd9f0867c4
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2022-10-25 19:27:47 +0530

    activity main and subject layout

---
+++ app/src/main/java/com/example/wisely/EngineeringMathematicsI.java
@@ -0,0 +1,14 @@
+package com.example.wisely;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EngineeringMathematicsI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_engineering_mathematics_i);
+    }
+}
