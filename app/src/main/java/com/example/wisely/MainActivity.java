94fea65bed7b2c566a3a270a4f8bb52c5feed0c1
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2022-10-25 12:51:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/wisely/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wisely;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.videos_subject_activity);
+    }
+}
