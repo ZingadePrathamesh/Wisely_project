242d9f5ed888adfb9396dfe358b82a1587a74ff8
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2022-10-26 10:53:31 +0530

    activity main and subject layout

---
+++ app/src/main/java/com/example/wisely/notesTypeSelection.java
@@ -0,0 +1,14 @@
+package com.example.wisely;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class notesTypeSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_type_selection);
+    }
+}
