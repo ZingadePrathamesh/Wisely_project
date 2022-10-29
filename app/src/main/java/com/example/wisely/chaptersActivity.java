d16d6dd21b39bff2f33112a1664b72a290dbfd3d
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2022-10-29 17:13:14 +0530

    hiii yede salim

--- app/src/main/java/com/example/wisely/chaptersActivity.java
+++ app/src/main/java/com/example/wisely/chaptersActivity.java
@@ -40,14 +40,14 @@ public class chaptersActivity extends AppCompatActivity {
     public  void em1 (){
         ListView listview = findViewById(R.id.listView);
         ArrayList<Chapter> list = new ArrayList<>();
-        list.add(new Chapter("chapter 1"));
-        list.add(new Chapter("chapter 2"));
-        list.add(new Chapter("chapter 3"));
-        list.add(new Chapter("chapter 4"));
-        list.add(new Chapter("chapter 5"));
-        list.add(new Chapter("chapter 6"));
-        list.add(new Chapter("chapter 7"));
-        list.add(new Chapter("chapter 8"));
+        list.add(new Chapter("Complex Number"));
+        list.add(new Chapter("Partial Diff."));
+        list.add(new Chapter("Application of P.D"));
+        list.add(new Chapter("Successive Diff."));
+        list.add(new Chapter("Matrices"));
+        list.add(new Chapter("Transcendental Eq."));
+        list.add(new Chapter("Linear Eq"));
+        list.add(new Chapter("Expansion of Func."));
         list.add(new Chapter("chapter 9"));
         list.add(new Chapter("chapter 10"));
 
@@ -62,42 +62,42 @@ public class chaptersActivity extends AppCompatActivity {
                 switch (i){
                     case 0:
                         Intent intent0  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent0.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent0.putExtra("keyname", "https://youtube.com/playlist?list=PLKS7ZMKnbPrSsOQDQOpYrDUp-4zQO8YFX");
                         startActivity(intent0);
                         break;
                     case 1:
                         Intent intent1  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent1.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent1.putExtra("keyname", "https://youtube.com/playlist?list=PLKS7ZMKnbPrQf7IBhKlsnLXwJRZSDMhun");
                         startActivity(intent1);
                         break;
                     case 2:
                         Intent intent2  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent2.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent2.putExtra("keyname", "https://youtube.com/playlist?list=PLVCBPCYGv7bC1JwOGH-X0FS-jTI4WRbI1");
                         startActivity(intent2);
                         break;
                     case 3:
                         Intent intent3  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent3.putExtra("keyname", "https://www.youtube.com/watch?v=yDEfU3iVSoA");
+                        intent3.putExtra("keyname", "https://youtube.com/playlist?list=PLKS7ZMKnbPrQDCA9bd-HG5VrGxW4ZZfLP");
                         startActivity(intent3);
                         break;
                     case 4:
                         Intent intent4  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent4.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent4.putExtra("keyname", "https://youtube.com/playlist?list=PLKS7ZMKnbPrT-Gy4k0r8jAr0mX2K32yyV");
                         startActivity(intent4);
                         break;
                     case 5:
                         Intent intent5  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent5.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent5.putExtra("keyname", "https://youtube.com/playlist?list=PLU6SqdYcYsfIk1VhXxIYNPFU67ym6gae8");
                         startActivity(intent5);
                         break;
                     case 6:
                         Intent intent6  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent6.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent6.putExtra("keyname", "https://youtube.com/playlist?list=PLU6SqdYcYsfK-Gk_te5_dgSoWyUlUMOUh");
                         startActivity(intent6);
                         break;
                     case 7:
                         Intent intent7  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent7.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent7.putExtra("keyname", "https://www.youtube.com/playlist?list=PLqx9kIanjwgY5eBjXNpmR4lWMa_M8S4mN");
                         startActivity(intent7);
                         break;
                     case 8:
@@ -117,12 +117,12 @@ public class chaptersActivity extends AppCompatActivity {
     public  void ep1 (){
         ListView listview = findViewById(R.id.listView);
         ArrayList<Chapter> list = new ArrayList<>();
-        list.add(new Chapter("chapter 1"));
-        list.add(new Chapter("chapter 2"));
-        list.add(new Chapter("chapter 3"));
-        list.add(new Chapter("chapter 4"));
-        list.add(new Chapter("chapter 5"));
-        list.add(new Chapter("chapter 6"));
+        list.add(new Chapter("Quantum Physics"));
+        list.add(new Chapter("Crystallography"));
+        list.add(new Chapter("SemiConductor"));
+        list.add(new Chapter("Interference"));
+        list.add(new Chapter("SuperCon. & SuperCap."));
+        list.add(new Chapter("Material & App."));
         list.add(new Chapter("chapter 7"));
         list.add(new Chapter("chapter 8"));
         list.add(new Chapter("chapter 9"));
@@ -139,32 +139,32 @@ public class chaptersActivity extends AppCompatActivity {
                 switch (i){
                     case 0:
                         Intent intent0  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent0.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent0.putExtra("keyname", "https://youtube.com/playlist?list=PLQzUXa8lZVq8Zciw0MsOdPJ1t-bpPKV6_");
                         startActivity(intent0);
                         break;
                     case 1:
                         Intent intent1  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent1.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent1.putExtra("keyname", "https://youtube.com/playlist?list=PLQzUXa8lZVq_3F1vB3NkQEZDzxt2g5SvF");
                         startActivity(intent1);
                         break;
                     case 2:
                         Intent intent2  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent2.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent2.putExtra("keyname", "https://youtube.com/playlist?list=PLQzUXa8lZVq8lfZN5836sohUpRCt2b6oB");
                         startActivity(intent2);
                         break;
                     case 3:
                         Intent intent3  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent3.putExtra("keyname", "https://www.youtube.com/watch?v=yDEfU3iVSoA");
+                        intent3.putExtra("keyname", "https://youtube.com/playlist?list=PLQzUXa8lZVq9raHeIhA8Cf5LuF3r2Pnu7");
                         startActivity(intent3);
                         break;
                     case 4:
                         Intent intent4  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent4.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent4.putExtra("keyname", "https://youtube.com/playlist?list=PLQzUXa8lZVq_DKTgVTq4LK-iSnviUFZIN");
                         startActivity(intent4);
                         break;
                     case 5:
                         Intent intent5  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent5.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent5.putExtra("keyname", "https://youtube.com/playlist?list=PLQzUXa8lZVq-GpWcleSQCxu1pczvgWTgJ");
                         startActivity(intent5);
                         break;
                     case 6:
@@ -194,12 +194,12 @@ public class chaptersActivity extends AppCompatActivity {
     public  void ec1 (){
         ListView listview = findViewById(R.id.listView);
         ArrayList<Chapter> list = new ArrayList<>();
-        list.add(new Chapter("chapter 1"));
-        list.add(new Chapter("chapter 2"));
-        list.add(new Chapter("chapter 3"));
-        list.add(new Chapter("chapter 4"));
-        list.add(new Chapter("chapter 5"));
-        list.add(new Chapter("chapter 6"));
+        list.add(new Chapter("Atomic and Mol. Struc."));
+        list.add(new Chapter("Aromatic System"));
+        list.add(new Chapter("Inter. Forces & Cric. Phen."));
+        list.add(new Chapter("Phase Rule"));
+        list.add(new Chapter("Polymers"));
+        list.add(new Chapter("Water"));
         list.add(new Chapter("chapter 7"));
         list.add(new Chapter("chapter 8"));
         list.add(new Chapter("chapter 9"));
@@ -216,32 +216,32 @@ public class chaptersActivity extends AppCompatActivity {
                 switch (i){
                     case 0:
                         Intent intent0  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent0.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent0.putExtra("keyname", "https://youtube.com/playlist?list=PL4BETMztLJZbw5PUGSH8u4GiZOt4NG64x");
                         startActivity(intent0);
                         break;
                     case 1:
                         Intent intent1  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent1.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent1.putExtra("keyname", "https://youtube.com/playlist?list=PLaySzQJTCO1mdBoL-BLBfKrhgdwRpbOnx");
                         startActivity(intent1);
                         break;
                     case 2:
                         Intent intent2  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent2.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent2.putExtra("keyname", "https://youtube.com/playlist?list=PLqx9kIanjwgZU0MeGEhNHuOJdp8EOAf0h");
                         startActivity(intent2);
                         break;
                     case 3:
                         Intent intent3  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent3.putExtra("keyname", "https://www.youtube.com/watch?v=yDEfU3iVSoA");
+                        intent3.putExtra("keyname", "https://youtube.com/playlist?list=PLLf6O8XdGj01keitLZ0scBnH80-MnNdu4");
                         startActivity(intent3);
                         break;
                     case 4:
                         Intent intent4  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent4.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent4.putExtra("keyname", "https://youtube.com/playlist?list=PLLf6O8XdGj006FPDExeXMKU5CcnHBn1mf");
                         startActivity(intent4);
                         break;
                     case 5:
                         Intent intent5  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent5.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent5.putExtra("keyname", "https://youtube.com/playlist?list=PLLf6O8XdGj03gLo6znlqJbMzgIgt8tSU1");
                         startActivity(intent5);
                         break;
                     case 6:
@@ -271,13 +271,13 @@ public class chaptersActivity extends AppCompatActivity {
     public  void bee (){
         ListView listview = findViewById(R.id.listView);
         ArrayList<Chapter> list = new ArrayList<>();
-        list.add(new Chapter("chapter 1"));
-        list.add(new Chapter("chapter 2"));
-        list.add(new Chapter("chapter 3"));
-        list.add(new Chapter("chapter 4"));
-        list.add(new Chapter("chapter 5"));
-        list.add(new Chapter("chapter 6"));
-        list.add(new Chapter("chapter 7"));
+        list.add(new Chapter("Prerequisite"));
+        list.add(new Chapter("DC circuits"));
+        list.add(new Chapter("AC circuits"));
+        list.add(new Chapter("Module 3"));
+        list.add(new Chapter("Transformers"));
+        list.add(new Chapter("Elec. Machines"));
+        list.add(new Chapter("Module 6"));
         list.add(new Chapter("chapter 8"));
         list.add(new Chapter("chapter 9"));
         list.add(new Chapter("chapter 10"));
@@ -293,37 +293,37 @@ public class chaptersActivity extends AppCompatActivity {
                 switch (i){
                     case 0:
                         Intent intent0  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent0.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent0.putExtra("keyname", "https://www.youtube.com/watch?v=Vd2UJiIPbag&list=PL9RcWoqXmzaLTYUdnzKhF4bYug3GjGcEc&index=1&t=16s");
                         startActivity(intent0);
                         break;
                     case 1:
                         Intent intent1  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent1.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent1.putExtra("keyname", "https://youtube.com/playlist?list=PLqx9kIanjwgYgayoOquclWga8OlnGOWN_");
                         startActivity(intent1);
                         break;
                     case 2:
                         Intent intent2  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent2.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent2.putExtra("keyname", "https://youtube.com/playlist?list=PLqx9kIanjwgZTJ-Ar-jkTuv34_XIXSBET");
                         startActivity(intent2);
                         break;
                     case 3:
                         Intent intent3  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent3.putExtra("keyname", "https://www.youtube.com/watch?v=yDEfU3iVSoA");
+                        intent3.putExtra("keyname", "https://youtube.com/playlist?list=PLqx9kIanjwgZWt9u6bjcTqlIf_fStX71u");
                         startActivity(intent3);
                         break;
                     case 4:
                         Intent intent4  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent4.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent4.putExtra("keyname", "https://youtube.com/playlist?list=PLohtAIfLLw8fFa2TGj3ZjUvS42nWFUQK3");
                         startActivity(intent4);
                         break;
                     case 5:
                         Intent intent5  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent5.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent5.putExtra("keyname", "https://youtube.com/playlist?list=PLohtAIfLLw8cdi1iOLyLNX1f839KSXDnl");
                         startActivity(intent5);
                         break;
                     case 6:
                         Intent intent6  = new Intent(getApplicationContext(),videoActivity.class);
-                        intent6.putExtra("keyname", "https://www.youtube.com/watch?v=50VNCymT-Cs");
+                        intent6.putExtra("keyname", "https://youtube.com/playlist?list=PLqx9kIanjwgb8_P6XinNSDMNo_qBeTpYz");
                         startActivity(intent6);
                         break;
                     case 7:
